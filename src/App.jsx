7c0e721d4ebcffac6304ba292bd622c05b8a6f41
Adam Bayraktar <adambayraktar@gmail.com>
2023-12-27 19:20:07 +0100

    Add code to display list of esential attributes of react

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,64 @@
-function App() {
+import reactIMG from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+
+function Header() {
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactIMG} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+    </div>
+  );
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+export function App() {
+  return (
+    <div>
+      <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            ></CoreConcepts>
+            <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            ></CoreConcepts>
+            <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            ></CoreConcepts>
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            ></CoreConcepts>
+          </ul>
+        </section>
+        <h2>Let's code!</h2>
       </main>
     </div>
   );
 }
 
-export default App;
+// export default App;
