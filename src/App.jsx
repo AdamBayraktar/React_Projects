a58f5edcc30aa77e145f1cd2e6fb5d30785523d1
Adam Bayraktar <adambayraktar@gmail.com>
2024-01-01 21:44:37 +0100

    Throw error message when added value is less than 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { Calculator } from "./components/Calculator.jsx";
 import { Header } from "./components/Header.jsx";
+import { ResultTable } from "./components/ResultTable.jsx";
+import { calculateInvestmentResults } from "./util/investment.js";
 
 function App() {
+  const [userInputs, setUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  const calculatedValues = calculateInvestmentResults(userInputs);
   return (
     <>
       <Header></Header>
-      <Calculator />
+      <Calculator userInputs={userInputs} setUserInput={setUserInput} />
+      <ResultTable calculatedValues={calculatedValues} />
     </>
   );
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,18 @@
 // if duration less than 1 then display info
 
-export function Calculator() {
+export function Calculator({ userInputs, setUserInput }) {
+  function onChangeHandler(event) {
+    if (event.target.value < 1) {
+      window.alert("The value cannot be less than 1!");
+      throw "exit";
+    }
+    setUserInput(() => {
+      let newUserInput = { ...userInputs };
+      newUserInput[event.target.name] = event.target.value;
+      return newUserInput;
+    });
+  }
+
   return (
     <div id="user-input">
       <div className="input-group">
@@ -11,6 +23,8 @@ export function Calculator() {
             id="initialInvestment"
             name="initialInvestment"
             min="1"
+            value={userInputs.initialInvestment}
+            onChange={onChangeHandler}
           />
         </label>
         <label for="annualInvestment">
@@ -20,6 +34,8 @@ export function Calculator() {
             id="annualInvestment"
             name="annualInvestment"
             min="1"
+            value={userInputs.annualInvestment}
+            onChange={onChangeHandler}
           />
         </label>
       </div>
@@ -31,11 +47,20 @@ export function Calculator() {
             id="expectedReturn"
             name="expectedReturn"
             min="1"
+            value={userInputs.expectedReturn}
+            onChange={onChangeHandler}
           />
         </label>
         <label for="duration">
           DURATION
-          <input type="number" id="duration" name="duration" min="1" />
+          <input
+            type="number"
+            id="duration"
+            name="duration"
+            min="1"
+            value={userInputs.duration}
+            onChange={onChangeHandler}
+          />
         </label>
       </div>
     </div>
