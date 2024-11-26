e5cebbf8a6f27a916d5520e5f656e1b5dbf7538b
Adam Bayraktar <adambayraktar@gmail.com>
2024-11-26 23:00:41 +0100

    FEAT: Modal shows you lost if the remaining seconds are below 0 and if it is above then shows you your score with the remaining secods

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,20 +1,25 @@
 import { forwardRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, handleReset },
   ref
 ) {
+  const userLost = remainingTime <= 0;
+  const formattedTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - formattedTime / targetTime) * 100);
+
   return (
     <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+      <h2>{userLost ? "You Lost" : `YOUR SCORE: ${score}`}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedTime} seconds left.</strong>
       </p>
       <form method="dialog">
-        <button>close</button>
+        <button onClick={handleReset}>close</button>
       </form>
     </dialog>
   );
